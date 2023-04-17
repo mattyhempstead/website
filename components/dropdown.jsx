92109bc52dab2c5627f29324f93ac59dd887a005
mattyhempstead <matty.hempstead@gmail.com>
2023-04-17 15:41:20 +1000

    Improve dropdown style

--- components/dropdown.jsx
+++ components/dropdown.jsx
@@ -2,29 +2,29 @@ import React, { useState } from 'react';
 import styles from './dropdown.module.css';
 
 const Dropdown = ({children}) => {
-  const [isOpen, setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
 
-  const handleClick = () => {
-    setIsOpen(!isOpen);
-  };
+    const handleClick = () => {
+        setIsOpen(!isOpen);
+    };
 
-  return (
-    <div className={styles.dropdown}>
-      <div className={styles.dropdownButton} onClick={handleClick}>
-        <span className={`${styles.dropdownIcon} ${isOpen ? styles.open : ''}`}>
-            {/* A right facing triangle */}
-            <svg viewBox="0 0 100 100">
-                <polygon points="10,0 90,50 10,100" />
-            </svg>
-        </span>
-      </div>
-      {isOpen && (
-        <div className={styles.dropdownContent}>
-            {children}
+    return (
+        <div className={styles.dropdown}>
+            <div className={styles.dropdownButton} onClick={handleClick}>
+                <span className={`${styles.dropdownIcon} ${isOpen ? styles.open : ''}`}>
+                    {/* A right facing triangle */}
+                    <svg viewBox="0 0 100 100">
+                        <polygon points="10,0 90,50 10,100" />
+                    </svg>
+                </span>
+            </div>
+            {isOpen && (
+                <div className={styles.dropdownContent}>
+                    {children}
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 };
 
 export default Dropdown;
