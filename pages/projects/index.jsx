aded582da9087d042ec5efa1b8188d2e62584e4f
mattyhempstead <matty.hempstead@gmail.com>
2023-12-23 10:06:12 +1100

    Convert projects list to be dynamically generated

--- pages/projects/index.jsx
+++ pages/projects/index.jsx
@@ -5,6 +5,7 @@ import Layout from '/components/layout';
 
 import styles from './index.module.css';
 
+import { PROJECTS_LIST } from './projectsList';
 
 
 const ProjectCard = ({ link, image, title, desc, date, builtWith }) => {
@@ -44,7 +45,7 @@ export default function Projects() {
 
                 <p>
                     An archive of some of the projects I mostly finished going all the way back to
-                    when I first taught myself to code in ~2015.
+                    when I first learned to code in ~2015.
 
                     <br/><br/>
 
@@ -60,7 +61,7 @@ export default function Projects() {
                     <br/><br/>
 
                     To ensure these projects are not lost to time and so I can look back at them
-                    years (decades?) from now, I thought I would put them all in one place.
+                    many years from now, I thought I would put them all in one place.
 
                     <br/><br/>
 
@@ -78,116 +79,17 @@ export default function Projects() {
 
 
 
-
                 <div className={styles.flexGrid}>
-                    <ProjectCard
-                        link="/projects/soil-shoveller"
-                        image="/projects/soil-shoveller/thumbnail.jpg"
-                        title="Soil Shoveller"
-                        desc="A really bad Cookie Clicker clone. Also probably one of the first things I ever coded."
-                        date="2015"
-                        builtWith="HTML + CSS + JS"
-                    />
-
-                    <ProjectCard
-                        link="/projects/ev1"
-                        image="/projects/ev1/thumbnail.png"
-                        title="Evolution Simulator v1"
-                        desc="The first version of my agent-based evolution simulations."
-                        date="2015"
-                        builtWith="HTML + CSS + JS"
-                    />
-
-                    <ProjectCard
-                        link="/projects/pong"
-                        image="/projects/pong/thumbnail.png"
-                        title="Pong"
-                        desc="My first pong."
-                        date="2015"
-                        builtWith="HTML + CSS + JS"
-                    />
-
-                    <ProjectCard
-                        link="/projects/cat-simulator-2015"
-                        image="/projects/cat-simulator-2015/thumbnail.jpg"
-                        title="Cat Simulator 2015"
-                        desc="very likely underwhelming"
-                        date="2015"
-                        builtWith="HTML + CSS + JS"
-                    />
-
-                    <ProjectCard
-                        link="/projects/superboyboy"
-                        image="/projects/superboyboy/thumbnail.png"
-                        title="Super Boy Boy"
-                        desc="A 2D platformer game from high school."
-                        date="2016"
-                        builtWith="Python + pygame"
-                    />
-
-                    <ProjectCard
-                        link="/projects/fb-soccer-python"
-                        image="/projects/fb-soccer-python/thumbnail.png"
-                        title="FB Soccer Python"
-                        desc="A python clone of the Facebook messenger soccer game."
-                        date="2016"
-                        builtWith="Python + pygame"
-                    />
-
-                    <ProjectCard
-                        link="/projects/fractal-tree"
-                        image="/projects/fractal-tree/thumbnail.png"
-                        title="Fractal Tree"
-                        desc="A procedural recursive tree generator."
-                        date="2017 or 2018 ???"
-                        builtWith="HTML + CSS + JS"
-                    />
-
-                    <ProjectCard
-                        link="/projects/image-circle-tiler"
-                        image="/projects/image-circle-tiler/thumbnail.png"
-                        title="Image Circle Tiler"
-                        desc="A programmatic art image circle effect."
-                        date="2017 or 2018 ???"
-                        builtWith="HTML + CSS + JS"
-                    />
-
-                    <ProjectCard
-                        link="/projects/image-pixel-sorter"
-                        image="/projects/image-pixel-sorter/thumbnail.png"
-                        title="Image Pixel Sorter"
-                        desc="Have you ever wondered what it looks like to sort all the pixels in an image for some reason?"
-                        date="2017 or 2018 ???"
-                        builtWith="HTML + CSS + JS"
-                    />
-
-                    <ProjectCard
-                        link="/projects/pi-estimator"
-                        image="/projects/pi-estimator/thumbnail.png"
-                        title="Pi Estimator"
-                        desc="A cool visualisation of one method to estimate π."
-                        date="2017 or 2018 ???"
-                        builtWith="HTML + CSS + JS"
-                    />
-
-                    <ProjectCard
-                        link="/projects/image-evolution"
-                        image="/projects/image-evolution/thumbnail.png"
-                        title="Image Evolution"
-                        desc="Genetic algorithm that evolves images made of triangles."
-                        date="2017 or 2018 ???"
-                        builtWith="HTML + CSS + JS"
-                    />
-
-                    <ProjectCard
-                        link="/projects/car-evolution-v1"
-                        image="/projects/car-evolution-v1/thumbnail.png"
-                        title="Car Evolution v1"
-                        desc="Genetic algorithm that evolves cars to drive around a track."
-                        date="August 2016"
-                        builtWith="Python + pygame"
-                    />
-
+                    {PROJECTS_LIST.map(project => (
+                        <ProjectCard
+                            link={project.link}
+                            image={project.image}
+                            title={project.title}
+                            desc={project.desc}
+                            date={project.date}
+                            builtWith={project.builtWith}
+                        />
+                    ))}
                 </div>
 
 
---
+++ pages/projects/projectsList.jsx
@@ -0,0 +1,99 @@
+
+export const PROJECTS_LIST = [
+    {
+        "link": "/projects/soil-shoveller",
+        "image": "/projects/soil-shoveller/thumbnail.jpg",
+        "title": "Soil Shoveller",
+        "desc": "A really bad Cookie Clicker clone. Also probably one of the first things I ever coded.",
+        "date": "2015",
+        "builtWith": "HTML + CSS + JS"
+    },
+    {
+        "link": "/projects/ev1",
+        "image": "/projects/ev1/thumbnail.png",
+        "title": "Evolution Simulator v1",
+        "desc": "The first version of my agent-based evolution simulations.",
+        "date": "2015",
+        "builtWith": "HTML + CSS + JS"
+    },
+    {
+        "link": "/projects/pong",
+        "image": "/projects/pong/thumbnail.png",
+        "title": "Pong",
+        "desc": "My first pong.",
+        "date": "2015",
+        "builtWith": "HTML + CSS + JS"
+    },
+    {
+        "link": "/projects/cat-simulator-2015",
+        "image": "/projects/cat-simulator-2015/thumbnail.jpg",
+        "title": "Cat Simulator 2015",
+        "desc": "very likely underwhelming",
+        "date": "2015",
+        "builtWith": "HTML + CSS + JS"
+    },
+    {
+        "link": "/projects/superboyboy",
+        "image": "/projects/superboyboy/thumbnail.png",
+        "title": "Super Boy Boy",
+        "desc": "A 2D platformer game from high school.",
+        "date": "2016",
+        "builtWith": "Python + pygame"
+    },
+    {
+        "link": "/projects/fb-soccer-python",
+        "image": "/projects/fb-soccer-python/thumbnail.png",
+        "title": "FB Soccer Python",
+        "desc": "A python clone of the Facebook messenger soccer game.",
+        "date": "2016",
+        "builtWith": "Python + pygame"
+    },
+    {
+        "link": "/projects/fractal-tree",
+        "image": "/projects/fractal-tree/thumbnail.png",
+        "title": "Fractal Tree",
+        "desc": "A procedural recursive tree generator.",
+        "date": "2017 or 2018 ???",
+        "builtWith": "HTML + CSS + JS"
+    },
+    {
+        "link": "/projects/image-circle-tiler",
+        "image": "/projects/image-circle-tiler/thumbnail.png",
+        "title": "Image Circle Tiler",
+        "desc": "A programmatic art image circle effect.",
+        "date": "2017 or 2018 ???",
+        "builtWith": "HTML + CSS + JS"
+    },
+    {
+        "link": "/projects/image-pixel-sorter",
+        "image": "/projects/image-pixel-sorter/thumbnail.png",
+        "title": "Image Pixel Sorter",
+        "desc": "Have you ever wondered what it looks like to sort all the pixels in an image for some reason?",
+        "date": "2017 or 2018 ???",
+        "builtWith": "HTML + CSS + JS"
+    },
+    {
+        "link": "/projects/pi-estimator",
+        "image": "/projects/pi-estimator/thumbnail.png",
+        "title": "Pi Estimator",
+        "desc": "A nice visualisation of one method to estimate π.",
+        "date": "2017 or 2018 ???",
+        "builtWith": "HTML + CSS + JS"
+    },
+    {
+        "link": "/projects/image-evolution",
+        "image": "/projects/image-evolution/thumbnail.png",
+        "title": "Image Evolution",
+        "desc": "Genetic algorithm that evolves images made of triangles.",
+        "date": "2017 or 2018 ???",
+        "builtWith": "HTML + CSS + JS"
+    },
+    {
+        "link": "/projects/car-evolution-v1",
+        "image": "/projects/car-evolution-v1/thumbnail.png",
+        "title": "Car Evolution v1",
+        "desc": "Genetic algorithm that evolves cars to drive around a track.",
+        "date": "August 2016",
+        "builtWith": "Python + pygame"
+    }
+];
