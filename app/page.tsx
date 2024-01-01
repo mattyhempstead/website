bac9032f7497443551f53945e96eef4b11a71ced
mattyhempstead <matty.hempstead@gmail.com>
2024-01-01 13:53:23 +1100

    Update home page

--- app/page.tsx
+++ app/page.tsx
@@ -12,23 +12,25 @@ export const metadata: Metadata = {
 
 export default function Page() {
     return <div>
-        <h1>Hello.</h1>
+        {/* <h1>Hello.</h1> */}
 
-        <Image
+        {/* <Image
             src="/images/alien-dance.gif"
             width={220/4}
             height={328/4}
             alt="Alien Dancing"
-        />
+        /> */}
 
-        <br />
+        {/* <br /> */}
 
         <p>
-            Welcome to the website that I am willing to show people.
+            hi stranger
         </p>
         <br/>
 
-        I am a
+        <p>
+            I am a
+        </p>
         <ul className='mt-2'>
             <li><Link href="#education">Student</Link></li>
             <li><Link href="#employment">Software Developer</Link></li>
@@ -57,8 +59,9 @@ export default function Page() {
         </p>
 
         <div>
-            {/* Dropdown */}
-            Why Computer Science?
+            <p>
+                Why Computer Science?
+            </p>
 
             <Dropdown>
                 I learned to code in my free time during high school and realised pretty quickly
@@ -74,30 +77,41 @@ export default function Page() {
             </Dropdown>
         </div>
 
-        <br/>
 
         <div>
-            Why Data Science?
+            <p>
+                Why Data Science?
+            </p>
 
             <Dropdown>
-                I enjoyed physics enough in high school that I was tempted to pick it.
+                <p className='mt-2'>
+                    I enjoyed physics enough in high school that I was tempted to pick it.
 
-                However, by that time I had spent a couple years playing with (ML-based)
-                AI which I could see as a rapidly improving extension
-                to <a href="https://karpathy.medium.com/software-2-0-a64152b37c35">software</a>.
-                I decided to go all in on software rather than split my degree in two.
+                    However, by that time I had spent a couple years playing with (ML-based)
+                    AI which I could see as a rapidly improving extension
+                    to <a href="https://karpathy.medium.com/software-2-0-a64152b37c35">software</a>.
+                    I decided to go all in on software and utility rather than split my degree in two.
+                </p>
 
-                <br/><br/>
+                <p>
+                    Similar to CS, during my degree I have learned the value of data science
+                    outside of what I originally knew.
+                    For DS, this means the ability to extract knowledge from observation
+                    and reason about its uncertainty (i.e. <i>science</i>).
+                </p>
 
-                Similar to CS, during my degree I have learned the value of data science
-                outside of what I originally knew.
-                For DS, this means the ability to extract knowledge from observation
-                and reason about its uncertainty (i.e. <i>science</i>).
+                <p>
+                    I actually still ended up doing a course in Physics during my first semester which
+                    inspired a couple <a
+                        href="/projects/double-pendulemon">
+                        personal projects
+                    </a>.
+                </p>
             </Dropdown>
         </div>
 
         <p>
-            If you are an employer and want to know how I am performing, some more details 
+            More details about my education
             are probably on my <a href="https://www.linkedin.com/in/matty-hempstead-89a980191/">LinkedIn</a>.
         </p>
 
@@ -108,9 +122,11 @@ export default function Page() {
         </h2>
 
         <div>
-            I've worked at a few pretty diverse companies alongside my university studies.
+            <p>
+                I've worked at a few pretty diverse companies alongside my university studies.
+            </p>
 
-            <ul>
+            <ul className='[&>li]:mb-4'>
                 {/* Add logos to the left of each point.
                 Canva can use real logo, envisage and others can use other logos. */}
 
@@ -118,17 +134,22 @@ export default function Page() {
                     A software contracting company.
 
                     <Dropdown>
-                        I think I learned more in my first job than my entire degree, at least
-                        regarding the skills for practical software development.
-                        <br/><br/>
-                        Here I did some frontend work (where I learned React and the Node ecosystem)
-                        and some backend database work (where I learned SQL and DBMS).
-                        <br/><br/>
-                        This was also my first exposure to using Git, Jira and other software 
-                        management tools. I was also persuaded into using Linux
-                        by my coworkers, which I still use to this day.
-                        <br/><br/>
-                        <i>React, Node, SQL, .NET, SaaS interaction</i>
+                        <p className='mt-2'>
+                            I think I learned during my first job out of high school than my entire degree,
+                            at least regarding the skills for practical software development.
+                        </p>
+                        <p>
+                            Here I did some frontend work (where I learned React and the Node ecosystem)
+                            and some backend database work (where I learned SQL and DBMS).
+                        </p>
+                        <p>
+                            This was also my first exposure to using Git, Jira and other software 
+                            management tools. I was also persuaded into using Linux
+                            by my coworkers, which I still use to this day.
+                        </p>
+                        <p>
+                            <i>React, Node, SQL, .NET, SaaS interaction</i>
+                        </p>
                     </Dropdown>
                 </li>
 
@@ -136,19 +157,24 @@ export default function Page() {
                     A medical research lab at University of Sydney.
                     
                     <Dropdown>
-                        In my second job I was one of the developers responsible for creating
-                        and managing software for a lab in the university.
-                        <br/><br/>
-                        The role of a lab is arguably to produce papers, and it turns out that such
-                        a job requirements the management of a number of different objects
-                        (grants, papers, people, collaborations, expenses). Not to mention any
-                        industry-specific software at each stage of the data pipelines.
-                        <br/><br/>
-                        My job primarily revolved around connecting data sources like spreadsheets
-                        (Google Sheets) and DBMSs which held these objects, and then building interfaces
-                        so that lab members could interact with the data in useful ways.
-                        <br/><br/>
-                        <i>Django, ORMs, SQL, Web Dev, Google Sheets (API), etc.</i>
+                        <p className='mt-2'>
+                            In my second job I was one of the developers responsible for creating
+                            and managing software for a lab in the university.
+                        </p>
+                        <p>
+                            The role of a lab is arguably to produce papers, and it turns out that such
+                            a job requires the management of a number of different objects
+                            (grants, papers, people, collaborations, expenses). Not to mention any
+                            industry-specific software at each stage of the data pipelines.
+                        </p>
+                        <p>
+                            My job primarily revolved around connecting data sources like spreadsheets
+                            (Google Sheets) and DBMSs which held these objects, and then building interfaces
+                            so that lab members could interact with the data in useful ways.
+                        </p>
+                        <p>
+                            <i>Django, ORMs, SQL, Web Dev, Google Sheets (API), etc.</i>
+                        </p>
                     </Dropdown>
                 </li>
 
@@ -156,20 +182,25 @@ export default function Page() {
                     A graphic design platform (Canva!)
                     
                     <Dropdown>
-                        This was my first internship and work experience at a large (&gt;1k&nbsp;employees)
-                        company. This was notable for me as the first role where I was responsible for a small
-                        portion of a huge system, rather than a large portion of a small system.
-                        <br/><br/>
-                        By this point I had work experience as both a frontend and backend developer,
-                        so I decided to apply for infrastructure engineer in order to broaden my
-                        knowledge and try something new. In the end I was placed on the Cloud Security
-                        team, which additionally allowed me to dip into the area of cybersecurity.
-                        <br/><br/>
-                        In the internship I worked on improving Canva's internally used secrets manager,
-                        and also performed some day-to-day maintenance of other Cloud Security software
-                        used by Canva.
-                        <br/><br/>
-                        <i>Golang, AWS, Secrets Managers (Vault by Hashicorp)</i>
+                        <p>
+                            This was my first internship and work experience at a large (&gt;1k&nbsp;employees)
+                            company. This was notable for me as the first role where I was responsible for a small
+                            portion of a huge system, rather than a large portion of a small system.
+                        </p>
+                        <p>
+                            By this point I had work experience as both a frontend and backend developer,
+                            so I decided to apply for infrastructure engineer in order to broaden my
+                            knowledge and try something new. In the end I was placed on the Cloud Security
+                            team, which additionally allowed me to dip into the area of cybersecurity.
+                        </p>
+                        <p>
+                            In the internship I worked on improving Canva's internally used secrets manager,
+                            and also performed some day-to-day maintenance of other Cloud Security software
+                            used by Canva.
+                        </p>
+                        <p>
+                            <i>Golang, AWS, Secrets Managers (Vault by Hashicorp)</i>
+                        </p>
                     </Dropdown>
 
                 </li>
@@ -186,31 +217,25 @@ export default function Page() {
             Enjoyment
         </h2>
 
-        I tend to spend much of my free time
-        
-        <ul>
-            <li>Making things</li>
-            <li>Thinking of more things to make</li>
-            <li>Learning stuff, often for the purpose of allowing me to make more things</li>
-            <li>Trying to improve my mindset, largely for the purpose of making more things</li>
+        <p>
+            I tend to spend much of my free time:
+        </p>
+
+        <ul className='mt-2'>
+            <li>Building things (mostly software)</li>
+            <li>Thinking of more things to build</li>
+            <li>Learning stuff, often for the purpose of allowing me to build more things</li>
+            <li>Trying to improve my mindset, largely for the purpose of building more things</li>
         </ul>
 
-        <br/>
-
-        <div>
+        <p>
             Historically I have been pretty hopeless at documenting what I make,
             but now that there appears to be more of a benefit from doing so I figured
-            I should <i>make</i> a website (this one) that attempts it.
-        </div>
-
-        <p>
-            To see projects that are more presentable, 
-            go <Link href="/projects">here</Link>.
+            I should make a website (this one) that attempts it.
         </p>
 
         <p>
-            To see an archive of historical projects,
-            go <Link href="/more-projects">here</Link>.
+            To see some of my projects, go <Link href="/projects">here</Link>.
         </p>
 
 
